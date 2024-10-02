550801f97519f1c757ed341d471f10dd90bc43d0
him200anshu <107635981+him200anshu@users.noreply.github.com>
2024-10-02 13:15:20 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(
   {
-  origin:["https://o-j-frontend.vercel.app/"],
+  origin:["https://o-j-frontend.vercel.app"],
   methods:["POST","GET"],
   credentials:true
   }
